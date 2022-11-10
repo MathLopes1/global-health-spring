c1febe091c2ef1d4e89222f071d4fd6057091573
MathLopes1 <iamatheuslopes@gmail.com>
2022-11-10 15:36:45 -0300

    feat: adding abstract repository

---
+++ src/main/java/com/global/health/repositories/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.global.health.repositories;
+
+import org.springframework.stereotype.Repository;
+
+import com.global.health.entities.AddressEntity;
+
+@Repository
+public interface AddressRepository extends AbstractRepository<AddressEntity> {}
--- src/main/java/com/global/health/repositories/PatientRepository.java
+++ src/main/java/com/global/health/repositories/PatientRepository.java
@@ -1,7 +1,8 @@
 package com.global.health.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.global.health.entities.PatientEntity;
 
-public interface PatientRepository extends JpaRepository<PatientEntity, Long> { }
+@Repository
+public interface PatientRepository extends AbstractRepository<PatientEntity> { }
